b293fae6f9f30c54942e40c26a3ac6a0b5993df7
Bien Xuan Huy <bienxhuy@gmail.com>
2025-06-07 16:03:01 +0700

    using db configuration from env file

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,17 @@
 import fnKnex from 'knex';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = fnKnex({
     client: 'mysql2',
     connection:
-        {
-            host: '127.0.0.1',
-            port: 3306,
-            user: 'root',
-            password: '',
-            database: 'epaperdb'
-        },
-    pool: {min: 0, max: 7}
+    {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    pool: { min: 0, max: 7 }
 });
 export default db
